4f5a807b70efd64f5c3ade4d478291704abac804
NotCuteButNew <bwd2386884119@gmail.com>
2023-03-07 03:11:41 -0800

    add server.cpp

--- src/chap03/src/server.cpp
+++ src/chap03/src/server.cpp
@@ -1,7 +1,20 @@
+#include "chap03/AddTwoInts.h"
+#include "ros/init.h"
+#include "ros/node_handle.h"
 #include "ros/ros.h"
+#include "ros/service_server.h"
 
-int main(int argc,char** argv)
-{
+typedef chap03::AddTwoInts adt;
 
-    return 0;
+bool add(adt::Response &res, const adt::Request &req) {
+  res.sum = req.a + req.b;
+  ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
+  ROS_INFO("sending back response: [%ld]", (long int)res.sum);
+}
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "server");
+  ros::NodeHandle n;
+  ros::ServiceServer service = n.advertiseService("add_two_ints", add);
+  ros::spin();
+  return 0;
 }
